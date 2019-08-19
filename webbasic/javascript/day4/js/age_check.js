be26a6854c944aa3e828d10b2ecc3e4b3ef2d402
yeon0997 <53138115+yeon0997@users.noreply.github.com>
2019-08-19 17:19:50 +0900

    20190819

---
+++ webbasic/javascript/day4/js/age_check.js
@@ -0,0 +1,5 @@
+var age = prompt("당신의 나이는?", "");
+        if(age >=20)
+            document.write("<p>" + "당신은 성인입니다." + "</p>");
+        else
+            document.write("<p>" + "당신은 미성년자입니다." + "</p>");
